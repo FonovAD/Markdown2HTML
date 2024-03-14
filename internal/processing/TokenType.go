33b5681748c90513f8e8d94dc547bd97735ee17b
Alexandr Fonov <FonovAD@Gmail.com>
2024-03-14 23:02:19 +0300

    feature TokenType in tokenType.go
    Added a token type for grouping a numbered list

--- internal/processing/TokenType.go
+++ internal/processing/TokenType.go
@@ -6,13 +6,14 @@ type TokenType struct {
 }
 
 var TokenTypeList = map[string]TokenType{
-	"SEMICOLON":    TokenType{"SEMICOLON", "[\\n]"},
-	"HEADING":      TokenType{"HEADING", "[#]{1,6}"},
-	"LINE":         TokenType{"LINE", "[=|-|*]{3,}"},
-	"LIST":         TokenType{"LIST", "[*|-|+]{1}"},
-	"NUMBEREDLIST": TokenType{"NUMBEREDLIST", "[\\d.]"},
-	"CODE":         TokenType{"CODE", "[`]"},
-	"CODEBLOCK":    TokenType{"CODEBLOCK", "[`]{3}"},
-	"WORD":         TokenType{"WORD", "\\w+"},
-	"SPACE":        TokenType{"SPACE", "[\\t|\\v|\\s]"},
+	"BLOCKNUMBEREDLIST": TokenType{"BLOCKNUMBEREDLIST", "[]"},
+	"SEMICOLON":         TokenType{"SEMICOLON", "[\\n]"},
+	"HEADING":           TokenType{"HEADING", "[#]{1,6}"},
+	"LINE":              TokenType{"LINE", "[=|-|*]{3,}"},
+	"LIST":              TokenType{"LIST", "[*|-|+]{1}"},
+	"NUMBEREDLIST":      TokenType{"NUMBEREDLIST", "[\\d.]"},
+	"CODE":              TokenType{"CODE", "[`]"},
+	"CODEBLOCK":         TokenType{"CODEBLOCK", "[`]{3}"},
+	"WORD":              TokenType{"WORD", "\\w+"},
+	"SPACE":             TokenType{"SPACE", "[\\t|\\v|\\s]"},
 }
