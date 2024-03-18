839e00499b3865d57d0d10719b7e97620e627e45
Alexandr Fonov <FonovAD@Gmail.com>
2024-03-18 21:30:08 +0300

    style TokenTypes in tokenType.go
    The name of the Token Type List has been
    changed to Token Types to avoid confusion.
    Added SecondTokenTypes for auxiliary tokens

--- internal/processing/TokenType.go
+++ internal/processing/TokenType.go
@@ -5,15 +5,18 @@ type TokenType struct {
 	regex string
 }
 
-var TokenTypeList = map[string]TokenType{
+var TokenTypes = map[string]TokenType{
+	"SEMICOLON":    TokenType{"SEMICOLON", "[\\n|\\v]"},
+	"HEADING":      TokenType{"HEADING", "[#]{1,6}"},
+	"LINE":         TokenType{"LINE", "[=|-|*]{3,}"},
+	"LIST":         TokenType{"LIST", "[*|-|+]{1}"},
+	"NUMBEREDLIST": TokenType{"NUMBEREDLIST", "[\\d.]"},
+	"CODE":         TokenType{"CODE", "[`]"},
+	"CODEBLOCK":    TokenType{"CODEBLOCK", "[`]{3}"},
+	"WORD":         TokenType{"WORD", "\\w+"},
+	"SPACE":        TokenType{"SPACE", "[ |\\t]{1,}"},
+}
+
+var SecondTokenTypes = map[string]TokenType{
 	"BLOCKNUMBEREDLIST": TokenType{"BLOCKNUMBEREDLIST", "[]"},
-	"SEMICOLON":         TokenType{"SEMICOLON", "[\\n]"},
-	"HEADING":           TokenType{"HEADING", "[#]{1,6}"},
-	"LINE":              TokenType{"LINE", "[=|-|*]{3,}"},
-	"LIST":              TokenType{"LIST", "[*|-|+]{1}"},
-	"NUMBEREDLIST":      TokenType{"NUMBEREDLIST", "[\\d.]"},
-	"CODE":              TokenType{"CODE", "[`]"},
-	"CODEBLOCK":         TokenType{"CODEBLOCK", "[`]{3}"},
-	"WORD":              TokenType{"WORD", "\\w+"},
-	"SPACE":             TokenType{"SPACE", "[\\t|\\v|\\s]"},
 }
